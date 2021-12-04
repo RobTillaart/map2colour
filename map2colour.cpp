93a9c96036d0276e81fef36a70e3a6b5c779f46e
rob tillaart <rob.tillaart@gmail.com>
2021-12-04 20:40:07 +0100

    improve

--- map2colour.cpp
+++ map2colour.cpp
@@ -28,24 +28,24 @@ uint32_t map2colour::map2RGB(float value)
 {
   int index = 1;
   float factor = 0;
-  uint8_t R = _R[0];
-  uint8_t G = _G[0];
-  uint8_t B = _B[0];
+  uint8_t R = _Red[0];
+  uint8_t G = _Green[0];
+  uint8_t B = _Blue[0];
 
   if ( (_values[0] < value) && (value <= _values[6] ))
   {
     while ((index < 7) && (_values[index] < value)) index++;
     factor = 1 - (_values[index] - value) / (_values[index] - _values[index-1]);
-    R = _R[index - 1] + factor * (_R[index] - _R[index - 1]);
-    G = _G[index - 1] + factor * (_G[index] - _G[index - 1]);
-    B = _B[index - 1] + factor * (_B[index] - _B[index - 1]);
+    R = _Red[index - 1] + factor * (_Red[index] - _Red[index - 1]);
+    G = _Green[index - 1] + factor * (_Green[index] - _Green[index - 1]);
+    B = _Blue[index - 1] + factor * (_Blue[index] - _Blue[index - 1]);
   }
-  uint32_t col = R;
-  col <<= 8;
-  col |= G;
-  col <<= 8;
-  col |= B;
-  return col;
+  uint32_t colour = R;
+  colour <<= 8;
+  colour |= G;
+  colour <<= 8;
+  colour |= B;
+  return colour;
 }
 
 
